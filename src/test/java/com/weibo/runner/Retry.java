24925942552cfbba014f5d82d8d265df10c8c35d
neven7 <hugang1010@163.com>
2016-04-27 14:48:42 +0800

    修改

--- src/test/java/com/weibo/runner/Retry.java
+++ src/test/java/com/weibo/runner/Retry.java
@@ -7,12 +7,12 @@ import java.lang.annotation.Target;
 
 import com.weibo.global.ParseProperties;
 
-// 重试次数，默认3
+// 重试次数，默认2
 // 方法会覆盖类，子类会覆盖父类
 
 // 重试默认值改用配置文件global.properties中 retry 字段指定
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD,ElementType.TYPE})
 public @interface Retry {
-   int value() default 3;
+   int value() default 2;
 }
